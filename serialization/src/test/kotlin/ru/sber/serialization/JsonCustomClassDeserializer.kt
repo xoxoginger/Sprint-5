aaa20a6b7191314af72e89ec1edee32934809687
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-11 15:25:23 +0300

    serialization +

--- serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
+++ serialization/src/test/kotlin/ru/sber/serialization/JsonCustomClassDeserializer.kt
@@ -1,6 +1,10 @@
 package ru.sber.serialization
 
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -20,5 +24,15 @@ class JsonCustomClassDeserializer {
         assertEquals("Иван", client.firstName)
         assertEquals("Иванов", client.lastName)
         assertEquals("Иванович", client.middleName)
+    }
+}
+
+class Client7Deserializer: StdDeserializer<Client7>(Client7::class.java) {
+    override fun deserialize(p0: JsonParser?, p1: DeserializationContext?): Client7 {
+        val node = p0?.codec?.readTree<JsonNode>(p0)
+        val parsedString = node?.get("client").toString().trim('"').split(" ")
+        return Client7( parsedString[1],
+                        parsedString[0],
+                        parsedString[2])
     }
 }
