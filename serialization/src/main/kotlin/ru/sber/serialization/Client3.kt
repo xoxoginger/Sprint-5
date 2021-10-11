cc7a00e0240ca9abaa3da42dc515e9f2acddfd06
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-11 13:44:03 +0300

    solving serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client3.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client3.kt
@@ -1,6 +1,7 @@
 package ru.sber.serialization
 
 import java.time.LocalDate
+import com.fasterxml.jackson.annotation.JsonFormat
 
 data class Client3(
     val firstName: String,
@@ -8,5 +9,6 @@ data class Client3(
     val middleName: String?,
     val passportNumber: String,
     val passportSerial: String,
+    @JsonFormat(pattern = "MM-dd-yyyy")
     val birthDate: LocalDate
 )
