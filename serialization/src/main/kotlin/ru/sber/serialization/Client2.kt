34d588ad91b39be8fd182ef1f5e1477ed60dee7d
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-11 13:43:03 +0300

    solving serialization

--- serialization/src/main/kotlin/ru/sber/serialization/Client2.kt
+++ serialization/src/main/kotlin/ru/sber/serialization/Client2.kt
@@ -1,8 +1,10 @@
 package ru.sber.serialization
 
 import java.time.LocalDate
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Client2(
+    @JsonProperty(value = "name")
     val firstName: String,
     val lastName: String,
     val middleName: String?,
